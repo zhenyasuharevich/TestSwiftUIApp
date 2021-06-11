799c0ac78a45def0ab0de488f63a428689c1c31b
Yauheni <zhenyasuhar2@gmail.com>
2021-06-11 13:44:59 +0200

    Initial Commit

---
+++ TestSwiftUIApp/TestSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIAppApp.swift
+//  TestSwiftUIApp
+//
+//  Created by Zhenya Suharevich on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
